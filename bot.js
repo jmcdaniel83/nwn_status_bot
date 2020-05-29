29e58c1802b651ad5c6319c5a2bca6fb4491562c
Jason McDaniel <jason.mcdaniel@sfile.com>
2020-05-29 17:21:27 -0500

    Updated the discord library to work with latest version.  Added the request library. Finished providing an embed message result.  + updated the discord.io => latest discord.js  + added the new 'request' library to fetch the JSON from NWNList  + updated the logger to only run INFO level and above  + added more constants related to the server we are interested in, icons for displaying results, and the NWNList server  + generating the new discord client object  + added new functions related to finding the server of interest. (since this is async, needed to duplicate it for the moment.)  + added new generate_fail_embed() function that will generate our embed message upon failure  + added new generate_success_embed() function to generate our embed message upon success  + added new get_server_status() function that perfroms all the steps necessary to fetch the records from NWNList and find the server we are interested in.  Then based on the results, will provide bad the success or failure embed message.  + utilizing the string formater to simplify strings with variables  + updated the use of the message and client to leverage the new API provided by discord.js  + on 'ready' and on 'message' method have been updated to match the latest API provided by discord.js    * on 'message' msg argument is now parsed correctly and will handle commands that start with !nwn - currently supporting '!nwn status'  + correctly logging in using the new discord client.

--- bot.js
+++ bot.js
@@ -1,48 +1,193 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
+var request = require('request');
 var auth = require('./auth.json');
 
 // configure the logger
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+    colorize: true,
+    json: false
 })
-logger.level = 'debug';
+logger.level = 'info';
+
+// server of interest
+var SERVER_NAME = 'Valeor Campaign';
+//var ICON_URL = 'https://imgur.com/x6i6bIp';
+var ICON_URL = 'https://steamuserimages-a.akamaihd.net/ugc/791986132470357926/BBD7082715E6747FADCC1F72466BBDF9AB2BB058/';
+
+// server list API
+var NWN2_LIST_SERVER = 'http://www.nwnlist.com/servers/NWN2';
+
+// testing the pinging of the server
+var SERVER_IP = 'vulcan-atx.ddns.net';
+var SERVER_PORT = 5121;
 
 // initialize the bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+var client = new Discord.Client();
+
+/// Will look for the server of interest that is defined for this instance.
+function find_server() {
+    var found_server = null;
+
+    // go to the url and search for our server
+    request.get({url: NWN2_LIST_SERVER, json: true}, function(error, response, body) {
+        logger.info(`status: ${response.statusCode}`);
+        if (error !== null) {
+            logger.error(`message: ${error}`);
+            return;
+        }
+
+        // look for our server
+        body.forEach(server => {
+            if (server.server_name === SERVER_NAME) {
+                logger.info(`found server!`);
+                found_server = server;
+                return;
+            }
+        });
+    });
+    return found_server;
+}
+
+/// Will generate the failure embed message.
+function generate_fail_embed() {
+    // otherwise we will generate the offline message
+    var server_details = [
+        {name: 'Status', value: `**OFF-LINE**`}
+    ];
+
+    // the embeded object
+    var embed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`${SERVER_NAME}`)
+        .setDescription('**Server Status**')
+        .setThumbnail(ICON_URL)
+        .addFields(server_details)
+        .setTimestamp()
+        .setFooter(`${SERVER_NAME}`);
+    return embed;
+}
+
+/// Will generate our successful embed message.
+function generate_success_embed(found_server) {
+    // collect our metadata
+    var active_count = found_server.active_player_count;
+    var max_count = found_server.maximum_player_count;
+    var build_id = found_server.build_number;
+    var min_lvl = found_server.minimum_level;
+    var max_lvl = found_server.maximum_level;
+
+    // the fields that we will generate
+    var server_details = [
+        {name: 'Status', value: `**ONLINE**`, inline: true},
+        {name: 'User Count', value: `${active_count} / ${max_count}`, inline: true},
+        {name: 'Level Details', value: `${min_lvl} / ${max_lvl}`, inline: true}
+    ];
+
+    // the embeded object
+    var embed = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setTitle(`${SERVER_NAME} [_Build: ${build_id}_]`)
+        .setDescription('**Server Status**')
+        .setThumbnail(ICON_URL)
+        .addFields(server_details)
+        .setTimestamp()
+        .setFooter(`${SERVER_NAME}`);
+    return embed;
+}
+
+/// Will fetch the server of interest that is mentioned above.  If found, then
+/// the embeded message will 
+function get_server_status(msg) {
+    var found_server = null;
+
+    // go to the url and search for our server
+    request.get({url: NWN2_LIST_SERVER, json: true}, function(error, response, body) {
+        logger.info(`status: ${response.statusCode}`);
+        if (error !== null) {
+            logger.error(`message: ${error}`);
+            return;
+        }
+
+        // look for our server
+        body.forEach(server => {
+            logger.debug(`current server: ${server.server_name}`);
+
+            if (server.server_name === SERVER_NAME) {
+                logger.info(`found server!`);
+                found_server = server;
+                return;
+            }
+        });
+
+        // establish the embed message
+        var embed = new Discord.MessageEmbed();
+        if (found_server !== null) {
+            embed = generate_success_embed(found_server);
+        }
+        else {
+            // otherwise we will generate our failed embed message
+            embed = generate_fail_embed();
+        }
+
+        // send the embed message
+        return msg.channel.send(embed);
+    });
+
+    return;
+}
 
 // bot methods
-bot.on('ready', function(event) {
+client.on('ready', () => {
     logger.info('Connected');
-    var botName = bot.username + ' - (' + bot.id + ')';
-    logger.info('Logged in as: ' + botName);
+    var botName = `${client.user.tag}`;
+    logger.info(`Logged in as: ${botName}`);
 })
 
-bot.on('message', function(user, userId, channelId, message, event) {
-    // our bot needs to know if it will execute a command
-    // it will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+client.on('message', msg => {
+    logger.info(`recieved ${msg} from user ${msg.author.username}...`);
+    logger.info(`content: ${msg.content}`);
 
-        args = args.splice(1);
+    var type = msg.content.substr(0, 1);
+    if (type === '!') {
+        // then we will get the arguments provided in order to process the
+        // request
+        var args = msg.content.substr(1).split(' ');
+        logger.info(`args: ${args}`);
+        var cmd = args[0];
+        logger.info(`cmd: ${cmd}`);
+
+        var spliced = args.splice(1);
+        logger.info(`spliced: ${spliced}`);
+
         switch(cmd) {
             // !ping
-            case 'ping': {
-                bot.sendMessage({
-                    to: channelId,
-                    message: 'pong!'
-                });
-            }
-            break;
+            case 'ping':
+                msg.channel.send('pong!');
+                break;
+            // !test
+            case 'nwn':
+                var second_cmd = '';
+                if (spliced.length == 1) {
+                    second_cmd = spliced[0];
+                }
 
-            // more handling of commands
+                switch(second_cmd) {
+                    case 'status':
+                        return get_server_status(msg);
+
+                    case 'setup':
+                        logger.info('need to add more details for setup...');
+                        break;
+                }
+            default:
+                logger.warn(`no handling of provided command: ${cmd}`);
         }
     }
 })
+
+// login
+client.login(auth.token);
 
 // EOF
